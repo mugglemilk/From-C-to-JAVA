10da0457469ff8e53af5bd1a10398c023447241e
mugglemilk <120041384+mugglemilk@users.noreply.github.com>
2023-04-25 13:39:52 +0700

    Add files via upload

---
+++ W1P3_NoDivideByZero.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class W1P3_NoDivideByZero {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+        double z = scan.nextDouble(); 
+        if(z != 0){
+            System.out.println(String.format("%.6f", (x+y)/z)); 
+        }
+        else{
+            System.out.println("cannot divide by zero");
+        }
+    } 
+}
